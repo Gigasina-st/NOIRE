6b015b0e6706cf1d4ce4f5632041e0ca48dbfee7
Gigasina-st <maghsoodisina6@gmail.com>
2026-09-21 04:02:22 +0330

    Wire admin route into app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; import './styles.css'; ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Admin from './Admin';
+import './styles.css';
+
+const isAdminPath = window.location.pathname.startsWith('/admin');
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>{isAdminPath ? <Admin/> : <App/>}</React.StrictMode>
+);
